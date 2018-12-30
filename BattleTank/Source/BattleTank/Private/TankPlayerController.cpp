cf1f278217bc86620f85c711157ad215df37fb78
PME\asrinivasan <asrinivasan@prattmiller.com>
2018-12-30 15:16:20 -0500

    created tank & player controller c++ class which currently returns if the controller is posessing a tank as a log

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+#include "BattleTank.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not posessing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController posessing %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
